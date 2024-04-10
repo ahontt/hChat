1ed40e7bd905b78b0044db3b70ca66e5e187b939
ahontt <165604078+ahontt@users.noreply.github.com>
2024-04-10 22:48:43 +0200

    Begin client implementation

--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -14,9 +14,22 @@ namespace Client
     public partial class Form1 : Form
     {
 
+        TcpClient _client;
+        byte[] _buffer = new byte[4096];
+
         public Form1()
         {
             InitializeComponent();
+
+            _client = new TcpClient();
+            _client.Connect("127.0.0.1", 55000);
+
+            _client.GetStream().BeginRead(_buffer, 0, _buffer.Length, Server_MessageReceived, null);
+        }
+
+        private void Server_MessageReceived(IAsyncResult ar)
+        {
+
         }
     }
 }
